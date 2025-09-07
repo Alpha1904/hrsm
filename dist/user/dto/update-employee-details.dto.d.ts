b99ab9a1cb3ffcda45ca0ee3a1411d6c58607da0
Alpha1904 <alphapmc86@gmail.com>
2025-09-07 21:22:23 +0100

    delete dist

--- dist/user/dto/update-employee-details.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { ContractType } from '@prisma/client';
-export declare class UpdateEmployeeDetailsDto {
-    position?: string;
-    department?: string;
-    seniority?: number;
-    contractEnd?: Date;
-    contractType?: ContractType;
-    managerId?: number;
-}
--- dist/user/dto/user-query.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { Role } from '@prisma/client';
-export declare class UserQueryDto {
-    site?: string;
-    role?: Role;
-    search?: string;
-    page?: number;
-    limit?: number;
-}
